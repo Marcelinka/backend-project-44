65007f02aa6b591c65fd649751b998417c0b6a01
Marcelinka <miss.knigomanka@gmail.com>
2023-10-31 04:41:10 +0600

    refactor: move getRandomInt to helpers

--- src/games/getBrainEvenQuestion.js
+++ src/games/getBrainEvenQuestion.js
@@ -1,15 +1,13 @@
+import getRandomInt from '../helpers/getRandomInt.js';
+
 const MIN_NUMBER = 1;
 const MAX_NUMBER = 100;
 
 const EVEN_ANSWER = 'yes';
 const ODD_ANSWER = 'no';
 
-function getRandomInt() {
-  return Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER);
-}
-
 export default function getBrainEvenQuestion() {
-  const number = getRandomInt();
+  const number = getRandomInt(MIN_NUMBER, MAX_NUMBER);
   const isEven = number % 2 === 0;
   const rightAnswer = isEven ? EVEN_ANSWER : ODD_ANSWER;
 
