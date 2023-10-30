4e2f9548a570182b7fa6b658529789b27d9d828e
Marcelinka <miss.knigomanka@gmail.com>
2023-10-31 04:53:30 +0600

    refactor: change order

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import getBrainEvenQuestion from '../src/games/getBrainEvenQuestion.js';
 import start from '../src/index.js';
+import getBrainEvenQuestion from '../src/games/getBrainEvenQuestion.js';
 
 const tip = 'Answer "yes" if the number is even, otherwise answer "no".';
 
