d02468acfdb2f300dbc98838bca69196a27edb57
Marcelinka <miss.knigomanka@gmail.com>
2023-10-31 04:53:42 +0600

    fix: include max number

--- src/helpers/getRandomInt.js
+++ src/helpers/getRandomInt.js
@@ -1,3 +1,11 @@
+/**
+ * Возвращает случайное число от min до max, включая максимальное число.
+ *
+ * @param {number} min - целое число, минимальное значение
+ * @param {number} max - целое число, максимальное значение
+ * @returns {number}
+ */
 export default function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+  const maxIncluded = max + 1; // включаем максимальное число
+  return Math.floor(Math.random() * (maxIncluded - min) + min);
 }
