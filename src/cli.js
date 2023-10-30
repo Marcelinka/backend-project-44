27991ba50b454126a4dd457653e398b47592fbeb
Marcelinka <miss.knigomanka@gmail.com>
2023-10-31 03:45:16 +0600

    transfer welcome message to greet

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line import/prefer-default-export
 export function greet() {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
