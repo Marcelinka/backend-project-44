b50d23062a41a3206f97d986a7573890b07ea7e1
Marcelinka <miss.knigomanka@gmail.com>
2023-11-09 23:38:47 +0600

    fix: isPrimeNumber некорректно работало на 4

--- src/helpers/isPrimeNumber.js
+++ src/helpers/isPrimeNumber.js
@@ -3,7 +3,7 @@ export default function isPrimeNumber(number) {
     return false;
   }
 
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
     }
